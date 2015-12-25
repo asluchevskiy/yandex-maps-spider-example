1651f1b550ed9543464b865bb427e2f5b01378cb
Arseniy Sluchevskiy <arseniy.sluchevskiy@gmail.com>
2015-12-25 03:37:32 +0300

    main.py fix

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from settings import *
 from app import YandexMapsSpider, Exporter
 
 logging.basicConfig(level=LOG_LEVEL)
-# YandexMapsSpider(thread_number=THREAD_COUNT).run()
+YandexMapsSpider(thread_number=THREAD_COUNT).run()
 Exporter().run()
